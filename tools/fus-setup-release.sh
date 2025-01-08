726471cf88c2d01a53c57b7df06f8e592273428d
Philipp Gerbach <gerbach@fs-net.de>
2025-01-08 16:54:42 +0100

    Improve fus-setup-release
    Apply changes suggested by shellchecker
    
    Add possibility to
    - Accept EULA automatically
    - Execute command after stetup
    - Automatically use setup-environment if buildir already exists

--- tools/fus-setup-release.sh
+++ tools/fus-setup-release.sh
@@ -3,97 +3,102 @@
 # This is a wrapper script for NXPs fsl_setup_release.sh.
 # It calls the fsl_setup_release script with the given parameters
 # and adds some F&S specific configurations to the build
-
-source ./yocto-f+s-utilities
+set -e
+. ./yocto-f+s-utilities
 
 # Get the command line parameters
-while getopts "b:m:h" setup_flag
+while getopts "b:c:efh" setup_flag
 do
     case $setup_flag in
         b) BUILD_DIR="$OPTARG";
 		;;
-		m) FS_MODE="$OPTARG";
+        c) COMMAND="$OPTARG";
+        ;;
+        e) EULA=1;
 		;;
-		h) SHOW_HELP="1";
+        f) FORCE="1";
+		;;
+        h) SHOW_HELP="1";
+		;;
+		*) SHOW_HELP="1";
+
     esac
 done
 
 if [ -n "$SHOW_HELP" ]; then
 	print_usage
 	SHOW_HELP=""
-	return 1
+	exit 1
 fi
 
 if [ -z "$DISTRO" ]; then
    	print_usage
-	return 1
+	exit 1
 fi
 
 if [ -z "$MACHINE" ]; then
    	print_usage
-	return 1
+	exit 1
 fi
 
 if [ -z "$BUILD_DIR" ]; then
    	BUILD_DIR=build-$MACHINE-$DISTRO
 fi
 
-BUILD_DIR_REALPATH="$PWD/$BUILD_DIR"
+BUILD_DIR_REALPATH="${PWD}/$BUILD_DIR"
 
 if [ -z "$OEROOT_DIR" ]; then
-	OEROOT_DIR=$PWD/sources/poky
-	if [ -e $PWD/sources/oe-core ]; then
-    	OEROOT_DIR=$PWD/sources/oe-core
+	OEROOT_DIR=${PWD}/sources/poky
+	if [ -e "${PWD}/sources/oe-core" ]; then
+    	OEROOT_DIR=${PWD}/sources/oe-core
 	fi
 fi
 # Path to fsl-setup-release.sh script
 FSL_SETUP_RELEASE=sources/meta-imx/tools/imx-setup-release.sh
 
-# Call NXPs fsl_setup_release script
-DISTRO=$DISTRO MACHINE=$MACHINE . $FSL_SETUP_RELEASE -b $BUILD_DIR
-
-# Add FuS-Layer
-echo "" >> $BUILD_DIR/conf/bblayers.conf
-echo "BBLAYERS += \" \${BSPDIR}/sources/meta-fus/meta-fus-bsp \"" >> $BUILD_DIR/conf/bblayers.conf
-echo "BBLAYERS += \" \${BSPDIR}/sources/meta-fus/meta-fus-sdk \"" >> $BUILD_DIR/conf/bblayers.conf
-
-# Determine root file system mode
-if [ "$FS_MODE" == "ro" ]
-then
-	echo "" >> $BUILD_DIR/conf/local.conf
-	echo "# Remove this to make your file system read-writeable:" >> $BUILD_DIR/conf/local.conf
-	echo "EXTRA_IMAGE_FEATURES += \"read-only-rootfs \"" >> $BUILD_DIR/conf/local.conf
-	echo
-	echo "Building read-only root file system"
+if [ -d "$BUILD_DIR" ] && [ "$FORCE" != "1" ]; then
+	. setup-environment "$BUILD_DIR"
 else
-	echo
-	echo "Building read/write root file system"
-fi
+	# Call NXPs fsl_setup_release script
+	DISTRO="$DISTRO" MACHINE="$MACHINE" . $FSL_SETUP_RELEASE -b "$BUILD_DIR"
 
-##
-# Run layer dependend init
-##
+	# Add FuS-Layer
+	echo "" >> "$BUILD_DIR/conf/bblayers.conf"
+	echo "BBLAYERS += \" \${BSPDIR}/sources/meta-fus/meta-fus-bsp \"" >> "$BUILD_DIR/conf/bblayers.conf"
+	echo "BBLAYERS += \" \${BSPDIR}/sources/meta-fus/meta-fus-sdk \"" >> "$BUILD_DIR/conf/bblayers.conf"
 
-OLD_PWD=$PWD
-cd ..
-LIST_OF_INIT_SCRIPTS=$(ls -d sources/*)
-cd $OLD_PWD
+	##
+	# Run layer dependend init
+	##
 
-for INIT_SCRIPT in $LIST_OF_INIT_SCRIPTS; do
+	OLD_PWD=${PWD}
+	cd ..
+	LIST_OF_INIT_SCRIPTS=$(ls -d sources/*)
+	cd "$OLD_PWD"
 
-	if [[ -e ../${INIT_SCRIPT}/scripts/fus_setup.sh ]]; then
-		echo "Following extra layer init run": "../${INIT_SCRIPT}/scripts/fus_setup.sh"
-		export BUILD_DIR=$BUILD_DIR
-		sh ../${INIT_SCRIPT}/scripts/fus_setup.sh $BUILD_DIR ${INIT_SCRIPT}/scripts
+	for INIT_SCRIPT in $LIST_OF_INIT_SCRIPTS; do
+
+		if [ -e "../${INIT_SCRIPT}/scripts/fus_setup.sh" ]; then
+			echo "Following extra layer init run": "../${INIT_SCRIPT}/scripts/fus_setup.sh"
+			export BUILD_DIR="$BUILD_DIR"
+			sh "../${INIT_SCRIPT}/scripts/fus_setup.sh" "$BUILD_DIR ${INIT_SCRIPT}/scripts"
+		fi
+	done
+
+	# make a source_env file
+	if [ ! -e source_env ]; then
+		echo "#!/bin/sh" >> source_env
+		echo "cd $OEROOT_DIR" >> source_env
+		echo "set -- $BUILD_DIR_REALPATH" >> source_env
+		echo ". ./oe-init-build-env > /dev/null" >> source_env
+		echo "echo \"Back to build project $(basename "$BUILD_DIR_REALPATH").\"" >> source_env
 	fi
-done
-
-# make a source_env file
-if [ ! -e source_env ]; then
-    echo "#!/bin/sh" >> source_env
-    echo "cd $OEROOT_DIR" >> source_env
-    echo "set -- $BUILD_DIR_REALPATH" >> source_env
-    echo ". ./oe-init-build-env > /dev/null" >> source_env
-    echo "echo \"Back to build project $(basename $BUILD_DIR_REALPATH).\"" >> source_env
 fi
-###
+
+# run command
+if [ -n "$COMMAND" ]; then
+	echo "Running: $COMMAND"
+	$COMMAND
+fi
+
+
