e1216dd9afc3c3a47028b777159223fe9e58399c
Philipp Gerbach <gerbach@fs-net.de>
2025-07-11 09:16:57 +0200

    Add flexible chromium and spdx support
    The new chromium needs a special rust version.
    We don't want to use this version by default and want to keep the layers
    added to the build by default as small as possible.
    So we add a possibility to add chromium support with the
    fus-setup-release script with --add-chromium flag.
    
    We also wanto to be able to add spdx generation to the build.
    This can be done by adding --add-spdx [organisation]

--- tools/fus-setup-release.sh
+++ tools/fus-setup-release.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # This is a wrapper script for NXPs fsl_setup_release.sh.
 # It calls the fsl_setup_release script with the given parameters
@@ -10,44 +10,87 @@ WORKDIR="$PWD"
 
 add_layer() {
 	local layer="$1"
-	if [ -d "$WORKDIR/sources/$layer" ]; then
-		# Add rauc layer to bblayers.conf
-		echo "BBLAYERS += \" \${BSPDIR}/sources/$layer \"" >> $BUILD_DIR/conf/bblayers.conf
+	local bblayers_entry="BBLAYERS += \" \${BSPDIR}/sources/$layer \""
+	local file="$BUILD_DIR/conf/bblayers.conf"
+
+	if [ -d "$WORKDIR/sources/$layer" ] && ! grep -Fxq "$bblayers_entry" $file; then
+		echo "$bblayers_entry" >> $file
 	fi
 }
-# Get the command line parameters
-while getopts "b:c:efh" setup_flag
-do
-    case $setup_flag in
-        b) BUILD_DIR="$OPTARG";
+add_config () {
+	local config="$1"
+	local file="$BUILD_DIR/conf/local.conf"
+
+	if ! grep -Fxq "$config" $file; then
+		echo "$config" >> $file
+	fi
+}
+
+add_chromium() {
+	if [ "$CHROMIUM" == "1" ]; then
+		add_layer "meta-browser/meta-chromium"
+		add_layer "meta-lts-mixins"
+		add_config "IMAGE_INSTALL:append = \" chromium-ozone-wayland\""
+	fi
+}
+
+add_spdx() {
+	if [ "$SPDX" == "1" ]; then
+		add_config "SPDX_ORG = \"${SPDX_ORG}\""
+		add_config "INHERIT += \"create-spdx \""
+		add_config "SPDX_PRETTY = \"1\""
+		add_config "SPDX_INCLUDE_SOURCES = \"1\""
+		add_config "SPDX_ARCHIVE_SOURCES = \"0\""
+		add_config "SPDX_ARCHIVE_PACKAGED = \"0\""
+	fi
+}
+
+
+parse_arguments() {
+    while [[ $# -gt 0 ]]; do
+        case $1 in
+            -h|--help)
+                print_usage
+				exit 0
 		;;
-        c) COMMAND="$OPTARG";
+            -b)
+                BUILD_DIR="$2"
+                shift # past argument
+                shift # past value
         ;;
-        e) EULA=1;
+            -c)
+                COMMAND="$2"
+                shift # past argument
+                shift # past value
 		;;
-        f) FORCE="1";
+            -e)
+                EULA=1
+                shift # past argument
 		;;
-		h) SHOW_HELP="1";
+            -f)
+                FORCE="1"
+                shift # past argument
 		;;
-		*) SHOW_HELP="1";
+            --add-chromium)
+                CHROMIUM="1"
+                shift # past argument
+            ;;
+            --add-spdx)
+                SPDX="1"
+		SPDX_ORG="$2"
+                shift # past argument
+                shift # past value
+            ;;
+            *)    # unknown option
+                echo "Unknown option: $1"
+                print_usage
+		exit 0
+            ;;
     esac
 done
+}
 
-if [ -n "$SHOW_HELP" ]; then
-	print_usage
-	SHOW_HELP=""
-	return 0
-fi
-
-if [ -z "$DISTRO" ]; then
-   	print_usage
-	return 0
-fi
-
-if [ -z "$MACHINE" ]; then
-   	print_usage
-	return 0
-fi
+parse_arguments "$@"
 
 if [ -z "$BUILD_DIR" ]; then
    	BUILD_DIR=build-$MACHINE-$DISTRO
@@ -66,8 +109,14 @@ FSL_SETUP_RELEASE=setup-environment
 
 if [ -d "$BUILD_DIR" ] && [ "$FORCE" != "1" ]; then
 	. setup-environment "$BUILD_DIR"
+	# Point to the current directory since the last command changed the directory to $BUILD_DIR
+	BUILD_DIR=.
 else
 
+	if [ -z "$DISTRO" ] || [ -z "$MACHINE" ]; then
+	   	print_usage
+		exit 0
+	fi
 	DISTRO="$DISTRO" MACHINE="$MACHINE" . ./$FSL_SETUP_RELEASE  "$BUILD_DIR"
 
 
@@ -89,9 +138,9 @@ else
 fi
 
 	echo >> conf/local.conf
-	echo "# Switch to Debian packaging and include package-management in the image" >> conf/local.conf
-	echo "PACKAGE_CLASSES = \"package_deb\"" >> conf/local.conf
-	echo "EXTRA_IMAGE_FEATURES += \"package-management\"" >> conf/local.conf
+	add_config "# Switch to Debian packaging and include package-management in the image"
+	add_config "PACKAGE_CLASSES = \"package_deb\""
+	add_config "EXTRA_IMAGE_FEATURES += \"package-management\""
 
 	if [ ! -e $BUILD_DIR/conf/bblayers.conf.org ]; then
 		cp $BUILD_DIR/conf/bblayers.conf $BUILD_DIR/conf/bblayers.conf.org
@@ -104,12 +153,12 @@ fi
 	add_layer "meta-fus/meta-fus-bsp"
 	add_layer "meta-fus/meta-fus-sdk"
 	add_layer "meta-fus-nboot"
-	add_layer "meta-rauc"
-	echo "DISTRO_FEATURES:append =  \" rauc\"" >> $BUILD_DIR/conf/local.conf
+	add_layer "meta-silex-fus"
+
+	add_config "DISTRO_FEATURES:append =  \" rauc\""
 	add_layer "meta-rauc"
 	add_layer "meta-fus-updater"
 	add_layer "meta-fus-updater-azure"
-	add_layer "meta-silex-fus"
 
 
 
@@ -151,10 +200,13 @@ fi
 	fi
 fi
 
+# Add configs, given by parameter
+add_chromium
+add_spdx
+
+set +e
 # run command
 if [ -n "$COMMAND" ]; then
 	echo "Running: $COMMAND"
 	$COMMAND
 fi
-
-set +e
